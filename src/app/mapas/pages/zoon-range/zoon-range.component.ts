72a76c78a44e41a9b4d84c4dabaa38beb9c7d7a0
Victor <VictorCarmona9035@hotmail.com>
2022-09-03 20:59:50 +0200

    311-Pagina Zoom-Range-Diseño y tarea

--- src/app/mapas/pages/zoon-range/zoon-range.component.ts
+++ src/app/mapas/pages/zoon-range/zoon-range.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-zoon-range',
@@ -10,6 +11,12 @@ export class ZoonRangeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const map = new mapboxgl.Map({
+      container: 'mapa', // container ID
+      style: 'mapbox://styles/mapbox/streets-v11', // style URL
+      center:[-6.0761996, 37.41285254336611],
+      zoom:15
+    });
   }
 
 }
